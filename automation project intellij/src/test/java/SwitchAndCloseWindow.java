2ef9b7bf6509030f4fb4789c56fe5bfff37eaa8a
Andreea Popaluca <andreea.popaluca@gmail.com>
2022-02-17 19:47:12 +0200

    Update SwitchAndCloseWindow.java

--- automation project intellij/src/test/java/SwitchAndCloseWindow.java
+++ automation project intellij/src/test/java/SwitchAndCloseWindow.java
@@ -3,6 +3,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 public class SwitchAndCloseWindow {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
@@ -10,10 +13,21 @@ public class SwitchAndCloseWindow {
         driver.get("https://formy-project.herokuapp.com/switch-window");
         driver.findElement(By.id("new-tab-button")).click();
         driver.findElement(By.id("new-tab-button")).click();
-        for (String tabone: driver.getWindowHandles() ){
+
+        ArrayList<String> newTb = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(newTb.get(0));
+        System.out.println("Current page title is"+driver.getTitle());
+
+        driver.switchTo().window(newTb.get(1));
+        System.out.println("Current page title is"+driver.getTitle());
+
+        driver.switchTo().window(newTb.get(2));
+        System.out.println("Current page title is"+driver.getTitle());
+
+        /*for (String tabone: driver.getWindowHandles() ){
             driver.switchTo().window(tabone);
             Thread.sleep(4000);
-        }
+        }*/
         driver.close();
     }
 }
